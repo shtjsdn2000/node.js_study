ed11eaf1d99d4e0df2098c651bacd3cd310a0057
SeonWoo Noh <shtjsdn2000@naver.com>
2023-12-08 14:36:21 +0900

    6~7.MongoDB와 서버 연결하려면

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const app = express()
 app.use(express.static(__dirname + '/public'))
 
 
-// app.listen : 내 컴퓨터 PORT하나 오픈하는 문법
-app.listen(8080,() => {
-    console.log('http://localhost:8080에서 서버 실행중')
 
-})
 
 // 8080port로 들어온 사람들에게 다음과 같은 내용을 보여줄것
 
@@ -23,7 +19,9 @@ app.get('/first_page',(req,res)=>{
 })
 
 app.get('/news',(req,res)=>{
-    res.send('오늘 비옴')
+    //위 주소에 접속하면 db에 저장해보자
+    db.collection('post').insertOne({title : '어쩌구'})
+    // res.send('오늘 비옴')
 })
 
 //함수 안에 들어가는 함수를 call back함수라고한다.
@@ -36,4 +34,22 @@ app.get('/shop',(req,res)=>{
 //내 소개용 html 페이지 보내주기
 app.get('/about',(req,res)=>{
     res.sendFile(__dirname + '/about.html')
+})
+
+//ep7.MongoDB와 서버 연결하려면
+//MongoDB사용하기 위한 기본 코드임
+const {MongoClient} = require('mongodb');
+let db;
+//'DB접속URL 만 채우면 DB연결 끝'
+//mongodb+srv://admin:<password>@cluster0.8vbaxkq.mongodb.net/?retryWrites=true&w=majority
+const url = 'mongodb+srv://admin:qwer1234@cluster0.8vbaxkq.mongodb.net/?retryWrites=true&w=majority';
+new MongoClient(url).connect().then((client)=>{
+    console.log("DB연결성공");
+    db = client.db('forum');
+    // app.listen : 내 컴퓨터 PORT하나 오픈하는 문법
+    app.listen(8080,() => {
+        console.log('http://localhost:8080에서 서버 실행중')
+})
+}).catch((err)=>{
+    console.log(err)
 })
